b1977cb6839c84d468125313b76cfb68a3cd334e
Aneeka Mangal <aneekamangal@gmail.com>
2023-11-05 16:08:34 +0530

    Navbar done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+"use-client";
+import React from "react";
+import MobileMenu from "./MobileMenu";
+import logo2 from "../assets/logo_2.svg";
+import Image from "next/image";
+import Link from "next/link";
 
-type Props = {}
-
-const NavBar = (props: Props) => {
+const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="z-0 flex text-[0.7rem] lg:text-[1rem] items-center justify-between max-w-6xl py-5 mx-10 xl:mx-auto ">
+      <div>
+        <Image src={logo2} />
+      </div>
 
-export default NavBar
+      <div className="items-center hidden space-x-8 font-medium sm:flex lg:space-x-14 text-black">
+        <Link href="about">
+          <p className="transition cursor-pointer hover:text-primary">About</p>
+        </Link>
+        <Link href="SIPCalculator">
+          <p className="transition cursor-pointer hover:text-primary">
+            SIP Calculator
+          </p>
+        </Link>
+        <Link href="portfolio">
+          <p className="transition cursor-pointer hover:text-primary">
+            Portfolio
+          </p>
+        </Link>
+        <Link href="footer">
+          <p className="transition cursor-pointer hover:text-primary">
+            Contact
+          </p>
+        </Link>
+      </div>
+      <div>
+        <Link href="">
+          <p className="font-medium px-10 py-4 text-white transition rounded-[80px] cursor-pointer bg-p_green-500 hover:bg-p_green-900">
+            Connect with us
+          </p>
+        </Link>
+      </div>
+
+      <div className="relative sm:hidden z-50">
+        <MobileMenu />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
